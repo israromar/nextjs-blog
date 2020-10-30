141a08f1ae459a91202d6d43cc85ba5fb73570a1
“israromar” <israromar@gmail.com>
2020-10-30 16:34:38 -0400

    updated

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,5 +3,5 @@
 export default (req, res) => {
   res.statusCode = 200;
   // res.json({ name: "John Doe" });
-  res.status(200).json({ test: "Hello" });
+  res.status(200).json({ test: "Hello world" });
 };
